7ba10baef142afa514db502f30bccde478268ab1
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-24 11:09:22 +0100

    renderDiceView created and tested

--- client/src/views/dice_view.js
+++ client/src/views/dice_view.js
@@ -4,12 +4,30 @@ const CreateAppend = require('../helpers/create_append.js');
 const DiceView = function (container) {
   this.container = container;
   this.resultView = null;
+  this.diceDiv = null;
 
 };
 
 
 DiceView.prototype.bindEvents = function () {
 console.log("diceView exists");
+this.renderDiceView();
+};
+
+DiceView.prototype.renderDiceView = function () {
+console.log("renderDiceView exists");
+const newDiv = new CreateAppend('div', '', this.container);
+newDiv.id = 'dice-view';
+this.diceDiv = newDiv;
+console.log('this.diceDiv:', this.diceDiv);
+
+const diceButton = new CreateAppend('input', '', this.diceDiv);
+diceButton.type = 'button';
+diceButton.value = 'dice';
+diceButton.id = 'dice-button';
+
+const result = new CreateAppend('div', '', this.diceDiv);
+result.id = 'dice-result-view';
 };
 
 module.exports = DiceView;
