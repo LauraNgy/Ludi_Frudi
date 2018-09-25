a12826def2af3ead1e6f64914f45e40c1b074d7e
GerCassa <gercassa@gmail.com>
2018-09-25 16:53:46 +0100

    fixed more merge issues

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -18,35 +18,7 @@ Game.prototype.getPlayers = function () {
   });
 };
 
-<<<<<<< HEAD
-Game.prototype.playTurns = function () {
 
-  while (this.winner === null) {
-    // console.log(this.players);
-    this.winner = this.players[0];
-    this.players.forEach( (player) => {
-      PubSub.subscribe('DiceView:dice-roll-result-loaded', (event) => {
-        const diceValue = event.detail;
-        // console.log(diceValue);
-    //     PubSub.subscribe('BoardView:pawn-selected', (event) => {
-    //       const pawnID = event.detail;
-    //
-    //       if (player.colour === pawnCopy) {
-    //         player.turn(pawnID, diceValue);
-    //         if (player.won === 4) {
-    //           this.winner = player;
-    //         }
-    //       }
-        // });
-      });
-    });
-  };
-};
-
-Game.prototype.testpublishing = function () {
-  PubSub.subscribe('BoardView:pawn-selected', (event) => {
-    // console.log(event.detail);
-=======
 Game.prototype.playTurn = function () {
   PubSub.subscribe('DiceView:dice-roll-result-loaded', (event) => {
     const diceValue = event.detail;
@@ -54,7 +26,7 @@ Game.prototype.playTurn = function () {
     console.log(thisPlayer);
     thisPlayer.turn(diceValue);
     this.players.push(thisPlayer);
->>>>>>> f5c48ac6507cbd5cf741a294161dbd5d359d2250
+
   });
 };
 ;
